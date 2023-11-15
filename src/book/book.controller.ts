2349619acf521a150b0b51a0898a21bc27bb0559
Haithem Hadj Ammar <108501803+HaithemHadjAmmar@users.noreply.github.com>
2023-11-15 14:39:09 +0100

    Update in the Controller

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import {Body, Controller,Post, Get } from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schema/book.schema';
 
 @Controller('books')
 export class BookController {
     constructor(private bookService: BookService) {}
+
+    @Post()
+    async createBook(
+        @Body()
+        book,
+    ): Promise<Book> {
+        return this.bookService.create(book);
+    } 
 
   @Get()
    async getAllBooks(): Promise<Book[]> {
