fab26fbc839058aefd9f23e0c1c9a557fa9a15a2
Haithem Hadj Ammar <108501803+HaithemHadjAmmar@users.noreply.github.com>
2023-11-17 14:17:43 +0100

    update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schema/user.schema';
 import { Model } from 'mongoose';
@@ -6,6 +6,7 @@ import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 import { SignPrivateKeyInput } from 'crypto';
 import { SignUpDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -29,5 +30,25 @@ export class AuthService {
         const token = this.jwtService.sign({ id: user._id})
 
         return { token }
+    }
+
+    async login(loginDto: LoginDto): Promise<{ token: string}> {
+        const { email, password } = loginDto;
+
+        const user = await this.userModel.findOne({ email })
+
+        if(!user) {
+            throw new UnauthorizedException('invalid email or password')
+        }
+
+        const isPasswordMatched = await bcrypt.compare(password, user.password)
+    
+        if(!isPasswordMatched) {
+            throw new UnauthorizedException('invalid email or password')
+        }
+
+        const token = this.jwtService.sign({ sign: user._id });
+        return { token }
+
     }
 }
