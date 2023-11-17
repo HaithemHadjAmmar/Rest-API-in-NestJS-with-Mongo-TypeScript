6c520c7d27590fa1511e20fbd669e2aacf5328d6
Haithem Hadj Ammar <108501803+HaithemHadjAmmar@users.noreply.github.com>
2023-11-17 14:41:27 +0100

    AUTH ENDTASK

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiParam, ApiQuery } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -12,5 +13,11 @@ export class AuthController {
     @ApiOperation({ summary: 'SignUp' })
     signUp(@Body() signUpDto: SignUpDto): Promise<{token: string}> {
         return this.authService.signUp(signUpDto);
+    }
+
+    @Post('/login')
+    @ApiOperation({ summary: 'login' })
+    Login(@Body() loginDto: LoginDto): Promise<{token: string}> {
+        return this.authService.login(loginDto);
     }
 }
