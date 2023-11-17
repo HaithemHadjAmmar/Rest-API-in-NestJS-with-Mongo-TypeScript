46c64fc95d7e5d0697ab6e2d6d82ab297a679bab
Haithem Hadj Ammar <108501803+HaithemHadjAmmar@users.noreply.github.com>
2023-11-17 10:27:48 +0100

    add Auth module in the project

---
+++ src/auth/schema/user.schema.ts
@@ -0,0 +1,16 @@
+import { Prop, Schema } from "@nestjs/mongoose";
+
+@Schema({
+     timestamps: true  
+})
+export class User {
+
+    @Prop()
+    name: string
+
+    @Prop()
+    email: string
+
+    @Prop()
+    password: string
+}
